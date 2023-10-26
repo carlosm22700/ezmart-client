3772c9cc23f1e4615fe50046aac31f4594f0ca3c
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-10-25 23:17:45 -0700

    wip: reformat code into auth page

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 // import { useNavigate } from "react-router-dom";
 
 // import SignUpForm from "../../components/SignUpForm/SignUpForm";
@@ -7,31 +8,59 @@ import { useState } from "react";
 export default function AuthPage() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            const result = await axios.get(
+                'http://127.0.0.1:8000/users/',
+                {
+                    auth: {
+                        username: username,
+                        password: password
+                    }
+                }
+            );
+            setData(result.data);
+            setError(null);
+        } catch (error) {
+            console.error('Error fetching data: ', error);
+            setError('Error fetching data');
+        } finally {
+            setLoading(false);
+        }
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        //TODO: Submit the form to server to authenticate the user
-
-        
+        //Submit the form to server to authenticate the user
+        fetchData();
     };
 
 
     return (
-        //create form that will submit to server when button is clicked
-        <form onSubmit={handleSubmit}>
-            <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)} // on change, set the username to the value of the input
-            />
-            <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)} // on change, set the password to the value of the input
-            />
-            <button type="submit">Login</button>
-        </form>
-    )
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+            {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+        </div>
+    );
 }
